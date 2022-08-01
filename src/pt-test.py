77ab608bf65e5285725beeeb7f23dc0ff1cdc9fb
Bob Kozdemba <bkozdemba@gmail.com>
2022-08-01 18:55:56 -0500

    Update pt-test.py

--- src/pt-test.py
+++ src/pt-test.py
@@ -5,8 +5,9 @@ def run_sum(size: int) -> None:
     ###CPU
     start_time = time.time()
     a = torch.ones(size, size)
-    for _ in range(10000):
+    for _ in range(1000):
         a += a
+    torch.cuda.synchronize()
     elapsed_time = time.time() - start_time
 
     print('CPU time = ',elapsed_time)
@@ -14,12 +15,13 @@ def run_sum(size: int) -> None:
     ###GPU
     start_time = time.time()
     b = torch.ones(size, size).cuda()
-    for _ in range(10000):
+    for _ in range(1000):
         b += b
+    torch.cuda.synchronize()
     elapsed_time = time.time() - start_time
 
     print('GPU time = ',elapsed_time)
 
-for size in 64, 128, 256, 512, 1024:
+for size in 64, 128, 256, 512, 1024, 2048, 4096:
     print(f'size = {size} x {size}')
     run_sum(size)
